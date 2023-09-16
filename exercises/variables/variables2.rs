d7f7f28f8baac5e53d216d6bc8a3987359809a28
RayYu-ChunleiYu <Ray.yu@whu.edu.cn>
2023-09-16 17:04:24 +0800

    My rustling

---
+++ exercises/variables/variables2.rs
@@ -0,0 +1,14 @@
+// variables2.rs
+// Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
+
+// I AM DONE
+
+fn main() {
+    let x: i32;
+    x = 8;
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
